0311dbd11bfcd441234dc6a868f9fc6ee79de6e6
Bryce DeSantis <potreader@hotmail.com>
2023-11-02 10:35:49 -0400

    Display weather info to page

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 async function getWeather() {
 	const APIKey = "2b2c0f94c91440c7be6173147230111"
+	const APIUrl = `https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=london`
 	try {
-		const response = await fetch(
-			`https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=london`,
-			{ mode: "cors" }
-		)
+		const response = await fetch(APIUrl, { mode: "cors" })
 		const weatherDate = await response.json()
+		displayWeather(weatherDate)
 		console.log(weatherDate)
 	} catch {}
 }
+
+function displayWeather(data) {
+	const container = document.querySelector(".container")
+	container.innerHTML = `
+	<h3>${data.current.condition.text}</h3>
+	<p>${data.current.humidity}</p>
+	<p>${data.current.temp_f}</p>
+	<p>${data.current.temp_c}</p>`
+	console.log(data.current.humidity)
+	console.log(data.current.temp_c)
+	console.log(data.current.temp_f)
+	console.log(data.current.condition.text)
+}
+
 getWeather()
