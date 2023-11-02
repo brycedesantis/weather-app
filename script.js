7d2e7725ffbe3636909bc4a235db763e0060f8e1
Bryce DeSantis <potreader@hotmail.com>
2023-11-02 13:55:52 -0400

    Add error logic

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-async function getWeather() {
+async function getWeather(location) {
 	const APIKey = "2b2c0f94c91440c7be6173147230111"
-	const APIUrl = `https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=london`
+	const APIUrl = `https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${location}`
+
+	const weatherInfo = document.querySelector(".weather-info")
 	try {
 		const response = await fetch(APIUrl, { mode: "cors" })
 		const weatherDate = await response.json()
 		displayWeather(weatherDate)
 		console.log(weatherDate)
-	} catch {}
+	} catch (error) {
+		weatherInfo.innerHTML = `<h3>Location not found <span class="material-symbols-outlined" id="location-error">
+		wrong_location
+		</span></h3>`
+	}
 }
 
 function displayWeather(data) {
-	const container = document.querySelector(".container")
-	container.innerHTML = `
-	<h3>${data.current.condition.text}</h3>
-	<p>${data.current.humidity}</p>
-	<p>${data.current.temp_f}</p>
-	<p>${data.current.temp_c}</p>`
-	console.log(data.current.humidity)
-	console.log(data.current.temp_c)
-	console.log(data.current.temp_f)
-	console.log(data.current.condition.text)
+	const weatherInfo = document.querySelector(".weather-info")
+	weatherInfo.innerHTML = `
+	<h1 id='city'>${data.location.name}</h1>
+	<h4 id='country'>${data.location.region}, ${data.location.country}</h4>
+	<h3>${data.current.condition.text} <img src='${data.current.condition.icon}' /></h3>
+	<p>Humidity: ${data.current.humidity}</p>
+	<p>Temperture: ${data.current.temp_f}°F</p>
+	<p>Feels like: ${data.current.feelslike_f}°F</p>
+	<p>Temperture: ${data.current.temp_c}°C</p>
+	<p>Feels like: ${data.current.feelslike_c}°C</p>`
 }
 
-getWeather()
+function getLocation() {
+	const search = document.querySelector("#search-icon")
+	search.addEventListener("click", () => {
+		const input = document.querySelector("input")
+		return getWeather(input.value)
+	})
+}
+
+getWeather("london")
+getLocation()
